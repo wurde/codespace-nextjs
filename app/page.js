947d827a4965f445f3ab76354cfc22bdb0f61adf
andybettisworth.eth <andy.bettisworth@gmail.com>
2023-03-05 23:52:26 +0000

    update tex

--- app/page.js
+++ app/page.js
@@ -9,8 +9,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+          I've made changes on an iPad!
         </p>
         <div>
           <a
